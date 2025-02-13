7b1378649c58289b7996c1d87873265fb2ce72cf
benjamin edigbe <deeprising4ilife@gmail.com>
2025-02-13 13:14:47 -0800

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require ( 'fs') ;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +26,32 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/id', idslipuser);
 app.use('/schools', viewshools);
+
+var accounts = JSON.parse(fs.readFileSync('./data.json','utf-8'));
+
+app.get('/:id', function (req, res,next) {
+  try{
+      var id = req.params.id;
+      var foundUser = accounts.find((data) => id === data.id );
+      console.log(foundUser);
+      if (foundUser ) {
+          //req.session.user = foundUser.pine;
+              res.render('result',{Name:foundUser.Aname.Name,Mname:foundUser.Aname.Mname,Surname:foundUser.Aname.Surname,
+                  NIN:foundUser.NIN,Gender:foundUser.Gender,Day:foundUser.Ddateofbirth.Day,Month:foundUser.Ddateofbirth.Month,
+                  Year:foundUser.Ddateofbirth.Year,Presentclass:foundUser.Presentclass,Bloodgroup:foundUser.Bloodgroup,
+                  State:foundUser.State,School:foundUser.School,HometownCommunity:foundUser.HometownCommunity,
+                  ParentPhoneNo:foundUser.ParentPhoneNo,ParentPhoneNo2:foundUser.ParentPhoneNo2,Picturepath:foundUser.client,
+                  Status:foundUser.Status,id:foundUser.id,Status:foundUser.Status,time:foundUser.time});
+          } else {
+              res.render('ddx');
+          }
+     
+  } catch{
+      res.send("Internal server error");
+      
+  }
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
